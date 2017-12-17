c5d24aff3f0bab66b6fe96b426f6f115a2730f34
sylvainhubsch <sylvain.hubsch@gmail.com>
2017-12-17 15:06:01 -0800

    Added definition of new records headers_bindings_keys and headers_bindings

--- include/rabbit.hrl
+++ include/rabbit.hrl
@@ -162,6 +162,9 @@
 -record(trie_edge, {exchange_name, node_id, word}).
 -record(trie_binding, {exchange_name, node_id, destination, arguments}).
 
+-record(headers_bindings_keys, {exchange, binding_id}).
+-record(headers_bindings, {exch_bind, binding_type, destinations, compiled_args, other_operators}).
+
 -record(listener, {node, protocol, host, ip_address, port, opts = []}).
 
 -record(runtime_parameters, {key, value}).
